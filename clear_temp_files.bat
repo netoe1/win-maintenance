d7f12dec4840c75ac55b35395a6e0cc8c332739b
netoe1 <elynetobr@outlook.com>
2024-06-20 00:53:08 -0300

    fixed bug

--- clear_temp_files.bat
+++ clear_temp_files.bat
@@ -4,5 +4,5 @@ cls
 
 del C:\Windows\Temp\*.* /f /q
 del C:\Windows\Prefetch\*.* /f /q
-del C:\Users\%USERNAME%\AppData\Local\Temp /f /q
-del C:\Users\%USERNAME%\Recent /f /q
+del C:\Users\%USERNAME%\AppData\Local\Temp\*.* /f /q
+del C:\Users\%USERNAME%\Recent\*.* /f /q
