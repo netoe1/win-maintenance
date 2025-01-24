2121837738df5535e060e325af8b15cb46a9d259
Ely Torres Neto <93855901+netoe1@users.noreply.github.com>
2025-01-24 14:15:15 -0300

    Delete clear_temp_files.bat

--- clear_temp_files.bat
+++
@@ -1,8 +0,0 @@
-rem You must run this file as admin!
-@echo off
-cls
-
-del C:\Windows\Temp\*.* /f /q
-del C:\Windows\Prefetch\*.* /f /q
-del C:\Users\%USERNAME%\AppData\Local\Temp\*.* /f /q
-del C:\Users\%USERNAME%\Recent\*.* /f /q
